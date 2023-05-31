eb4f7ec0d887ee6ed7ddaed22ba47ba8b1766131
webRofl <maltsev_fe@mail.ru>
2023-05-31 15:43:27 +0400

    feat: update readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-readme
+### Инициализация
+```
+ $ ./project.sh init
+```
+
+### Запуск
+```
+ $ ./project.sh start
+```
+
+### Удалить все
+```
+ $ ./project.sh drop
+```
+
+### Создать и выполнить миграции
+```
+ $ ./project.sh migrate
+```
--- project.sh
+++ project.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 COMMAND=${1}
 
+# create migrations and migrate
 migrate() {
   docker compose run -exec --rm django python manage.py makemigrations
   docker compose run -exec --rm django python manage.py migrate
 }
 
+# kill all volumes, networks, images and containers
+drop() {
+  docker compose down -v
+}
+
+# setup project
+init() {
+  docker compose build
+  migrate
+  docker compose run -exec --rm django python manage.py createsuperuser
+}
+
+start() {
+  docker compose up
+}
+
 case $COMMAND in
 migrate)
   migrate
+  ;;
+drop)
+  drop
+  ;;
+init)
+  init
+  ;;
+start)
+  start
   ;;
 *)
   echo 'No action specified!'
